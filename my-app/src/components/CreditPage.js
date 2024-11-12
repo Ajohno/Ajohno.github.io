f72d9dea14ac243f2f9ac8795449fe197f0e7f45
Ajohno <44455099+Ajohno@users.noreply.github.com>
2024-11-12 15:08:02 -0500

    Set up react

---
+++ my-app/src/components/CreditPage.js
@@ -0,0 +1,13 @@
+// src/components/CreditPage.js
+import React from 'react';
+
+function CreditPage() {
+  return (
+    <div>
+      <h2>CreditPage</h2>
+      <p>This is the CreditPage.</p>
+    </div>
+  );
+}
+
+export default CreditPage;
---
+++ my-app/src/components/HomePage.js
@@ -0,0 +1,13 @@
+// src/components/HomePage.js
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <p>Welcome to the homepage!</p>
+    </div>
+  );
+}
+
+export default HomePage;
